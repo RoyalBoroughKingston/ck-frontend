c861ce28d6085e21004d0f68ee1d8815e26896db
Sean Jessen <info@seanjessen.co.uk>
2018-08-08 11:58:30 +0100

    Colors and Typography

--- index.php
+++ index.php
@@ -4,14 +4,17 @@
 	<title>Ayup Development boilerplate</title>
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
-  <link rel="canonical" href="Boilerplate" />
+	<link rel="canonical" href="Boilerplate" />
 
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-  <meta name="apple-mobile-web-app-capable" content="yes" />
-  <meta name="format-detection" content="telephone=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="format-detection" content="telephone=no">
 
+	<!-- Typekit -->
+	<link rel="stylesheet" href="https://use.typekit.net/bjq5bor.css">
+	
 	<!-- Site styles -->
-  <link href="./assets/dist/app.css" media="screen, projection" rel="stylesheet" />
+	<link href="./assets/dist/app.css" media="screen, projection" rel="stylesheet" />
 </head>
 
 <body>
@@ -22,32 +25,7 @@
 	<main id="content" class="page" role="main">
 		<section class="section">
 			<div class="flex-container">
-				<?php include('templates/_components/card.html'); ?>
-			</div>
-		</section>
-
-		<section class="section">
-			<div class="flex-container">
-				<?php include('templates/_components/accordion.html'); ?>
-				<?php include('templates/_components/accordion.html'); ?>
-				<?php include('templates/_components/accordion.html'); ?>
-				<?php include('templates/_components/accordion.html'); ?>
-				<?php include('templates/_components/accordion.html'); ?>
-				<?php include('templates/_components/accordion.html'); ?>
-			</div>
-		</section>
-
-		<section class="section section--grey">
-			<div class="flex-container">
-				<div class="flex-col flex-col--4 flex-col--medium--6">
-					<?php include('templates/_components/cta.html'); ?>
-				</div>
-				<div class="flex-col flex-col--4 flex-col--medium--6">
-					<?php include('templates/_components/cta.html'); ?>
-				</div>
-				<div class="flex-col flex-col--4 flex-col--medium--12">
-					<?php include('templates/_components/cta.html'); ?>
-				</div>
+				
 			</div>
 		</section>
 	</main>
