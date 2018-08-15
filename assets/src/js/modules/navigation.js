2c48ccd6e4933413aed87856fcf6e82cb30f65d5
Sean Jessen <info@seanjessen.co.uk>
2018-08-15 12:23:51 +0100

    Responsive header/Mobile action closes #24

--- assets/src/js/modules/navigation.js
+++ assets/src/js/modules/navigation.js
@@ -2,10 +2,12 @@ export class Navigation {
 
   constructor() {
 
-    let navContainer = document.querySelector('.nav--primary');
+    let navContainer = document.querySelector('.header');
     let navTrigger = document.querySelector('.nav-trigger');
 
     navTrigger.addEventListener('click', evt => {
+      console.log('trigger');
+      navTrigger.classList.toggle('active');
       navContainer.classList.toggle('active');
     });
 
