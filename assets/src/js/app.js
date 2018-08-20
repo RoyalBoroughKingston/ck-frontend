bac6f8dfce9a6911655b72b1cfd56d9e29808b59
Sean Jessen <info@seanjessen.co.uk>
2018-08-20 11:13:37 +0100

    Added favicons closes #27

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,11 +1,7 @@
-import { Accordion } from "./modules/accordion.js";
-import { Modal } from "./modules/modal.js";
 import { Navigation } from "./modules/navigation.js";
 
 export class App {
   constructor() {
-    let accordion = new Accordion();
-    let modal = new Modal();
     let navigation = new Navigation();
   }
 }
