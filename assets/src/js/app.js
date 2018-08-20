7d24a21302f6b06ae5d3e4650fc68f20a3577e44
Sean Jessen <info@seanjessen.co.uk>
2018-08-20 12:15:23 +0100

    Updated form components

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,7 +1,9 @@
+import { Form } from "./modules/form.js";
 import { Navigation } from "./modules/navigation.js";
 
 export class App {
   constructor() {
+    let form = new Form();
     let navigation = new Navigation();
   }
 }
--- assets/src/js/modules/form.js
+++ assets/src/js/modules/form.js
@@ -1,10 +1,15 @@
-import validation from './modules/validation';
+import validation from '../modules/validation';
+import Selectr from '../../../../node_modules/mobius1-selectr/dist/selectr.min.js';
 
 export class Form {
 
   constructor() {
 
-    
+    let selectLists = document.querySelectorAll('.select');
+
+    selectLists.forEach((evt) => {
+      new Selectr('.select');
+    });
 
   }
   
