6b069c532a3213c992f83d3d61aba846b20c7de7
Sean Jessen <info@seanjessen.co.uk>
2018-08-22 21:16:32 +0100

    Contact page and styling closes #38

--- templates/_components/breadcrumbs.php
+++ templates/_components/breadcrumbs.php
@@ -2,30 +2,32 @@
     $crumbs = explode("/",$_SERVER["REQUEST_URI"]);
 ?>
 
-<nav class="breadcrumbs">
-    <a href="/" class="breadcrumbs__crumb">Home</a>
+<?php if($_SERVER["REQUEST_URI"] !== '/'): ?>
+    <nav class="breadcrumbs">
+        <a href="/" class="breadcrumbs__crumb">Home</a>
 
-    <?php 
-    	 
-        $crumbs = explode("/",$_SERVER["REQUEST_URI"]);
+        <?php 
+            
+            $crumbs = explode("/",$_SERVER["REQUEST_URI"]);
 
-        $linkPath = "";
+            $linkPath = "";
 
-        $numItems = count($crumbs);
+            $numItems = count($crumbs);
 
-        foreach($crumbs as $index => $crumb){
-            if($index == 0) {
-                continue;
+            foreach($crumbs as $index => $crumb){
+                if($index == 0) {
+                    continue;
+                }
+
+                $word = ucfirst($crumb);
+
+                if(++$index === $numItems) {
+                    echo '<span class="breadcrumbs__crumb breadcrumbs__crumb--current">' . $word . '</span>';
+                } else {
+                    echo '<a href="' . $linkPath . $crumb . '" class="breadcrumbs__crumb">' . $word . '</a>';
+                    $linkPath = $linkPath . $crumb . '/';
+                }
             }
-
-            $word = ucfirst($crumb);
-
-        	if(++$index === $numItems) {
-	            echo '<span class="breadcrumbs__crumb breadcrumbs__crumb--current">' . $word . '</span>';
-        	} else {
-                echo '<a href="' . $linkPath . $crumb . '" class="breadcrumbs__crumb">' . $word . '</a>';
-                $linkPath = $linkPath . $crumb . '/';
-            }
-        }
-    ?>
-</nav>
+        ?>
+    </nav>
+<?php endif; ?>
---
+++ templates/pages/contact/index.php
@@ -0,0 +1,48 @@
+<section class="section section--header section--header--1 section--reduce-padding">
+    <div class="flex-container flex-container--space">
+        <div class="flex-col flex-col--5">
+            {% include('templates/_components/title-card.php') with { title: 'Contact us' } %}
+        </div>
+    </div>
+</section>
+
+<section class="section">
+    <div class="flex-container flex-container--space">
+        <div class="flex-col flex-col--7">
+            <div class="section__component">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in odio massa. Nullam tincidunt orci vitae vulputate aliquam. Etiam finibus ultricies arcu, non tempor lacus condimentum quis.</p>
+            </div>
+
+            <div class="section__component">
+                <p><a href="#" class="link link--icon-after">Would you like to be a volunteer? <i class="fa fa-angle-right"></i></a></p>
+                <p><a href="#" class="link link--icon-after">Would you like to leave feedback? <i class="fa fa-angle-right"></i></a></p>
+            </div>
+        </div>
+
+        <div class="flex-col flex-col--3">
+            {% include('templates/_components/service.php') with { type: 'contact', name: 'Contact', description: 'Fresh Start is an early intervention, homelessness prevention programme which is outreach and community...', telephone: '01245 360803', email: 'info@cwkingston.com', enableSocial: 'true', facebook: '#', twitter: '#' } %}
+        </div>
+    </div>
+</section>
+
+<section class="section section--social-media">
+    <div class="flex-container">
+        <div class="flex-col flex-col--12">
+            <h3>Hear from us</h3>
+        </div>
+    </div>
+
+    <div class="flex-container text-center">
+        <div class="flex-col flex-col--4">
+            {% include('templates/_components/card.php') with { type: 'social', frame: 'white', platform: 'twitter' } %}
+            {% include('templates/_components/button.php') with { type: 'secondary', action: '#', target: '_blank', text: 'Follow us on twitter' } %}
+        </div>
+
+        <div class="flex-col flex-col--2"></div>
+
+        <div class="flex-col flex-col--4">
+            {% include('templates/_components/card.php') with { type: 'social', frame: 'white', platform: 'facebook' } %}
+            {% include('templates/_components/button.php') with { type: 'secondary', action: '#', target: '_blank', text: 'Like us on facebook' } %}
+        </div>
+    </div>
+</section>
