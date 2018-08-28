c153f79cd93c40509a95f63f19474c41107b04fd
Sean Jessen <info@seanjessen.co.uk>
2018-08-28 17:27:04 +0100

    Ammended breadcrumb function closes #45

--- templates/_components/breadcrumbs.php
+++ templates/_components/breadcrumbs.php
@@ -1,40 +1,43 @@
-<div class="breadcrumbs">
-    <?php
+<?php if($_SERVER['REQUEST_URI'] !== '/'): ?>
+    <div class="breadcrumbs">
+        <?php
 
-    // This function will take $_SERVER['REQUEST_URI'] and build a breadcrumb based on the user's current path
-    function breadcrumbs($home = 'Home') {
-        // This gets the REQUEST_URI (/path/to/file.php), splits the string (using '/') into an array, and then filters out any empty values
-        $path = array_filter(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
-    
-        // This will build our "base URL" ... Also accounts for HTTPS :)
-        $base = ($_SERVER['HTTPS'] ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
-    
-        // Initialize a temporary array with our breadcrumbs. (starting with our home page, which I'm assuming will be the base URL)
-        $breadcrumbs = Array("<a href=\"$base\" class=\"breadcrumbs__crumb\">$home</a>");
-    
-        // Find out the index for the last value in our path array
-        $last = end(array_keys($path));
-    
-        // Build the rest of the breadcrumbs
-        foreach ($path AS $x => $crumb) {
-            // Our "title" is the text that will be displayed (strip out .php and turn '_' into a space)
-            $title = ucwords(str_replace(Array('.php', '_'), Array('', ' '), $crumb));
-    
-            // If we are not on the last index, then display an <a> tag
-            if ($x != $last)
-                $breadcrumbs[] = "<a href=\"$base$crumb\" class=\"breadcrumbs__crumb\">$title</a>";
-            // Otherwise, just display the title (minus)
-            else
-                $breadcrumbs[] = $title;
+        // This function will take $_SERVER['REQUEST_URI'] and build a breadcrumb based on the user's current path
+        function breadcrumbs($home = 'Home') {
+            // This gets the REQUEST_URI (/path/to/file.php), splits the string (using '/') into an array, and then filters out any empty values
+            $path = array_filter(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
+        
+            // This will build our "base URL" ... Also accounts for HTTPS :)
+            $base = ($_SERVER['HTTPS'] ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
+        
+            // Initialize a temporary array with our breadcrumbs. (starting with our home page, which I'm assuming will be the base URL)
+            $breadcrumbs = Array("<a href=\"$base\" class=\"breadcrumbs__crumb\">$home</a>");
+        
+            // Find out the index for the last value in our path array
+            $keys = array_keys($path);
+            $last = end($keys);
+        
+            // Build the rest of the breadcrumbs
+            foreach ($path AS $x => $crumb) {
+                // Our "title" is the text that will be displayed (strip out .php and turn '_' into a space)
+                $title = ucwords(str_replace(Array('.php', '_'), Array('', ' '), $crumb));
+        
+                // If we are not on the last index, then display an <a> tag
+                if ($x != $last)
+                    $breadcrumbs[] = "<a href=\"$base$crumb\" class=\"breadcrumbs__crumb\">$title</a>";
+                // Otherwise, just display the title (minus)
+                else
+                    $breadcrumbs[] = $title;
+            }
+        
+            // Build our temporary array (pieces of bread) into one big string :)
+            return implode($breadcrumbs);
         }
-    
-        // Build our temporary array (pieces of bread) into one big string :)
-        return implode($breadcrumbs);
-    }
-    
-    ?>
-    
-    <?php
-        echo breadcrumbs() ;
-    ?>
-</div>
+        
+        ?>
+        
+        <?php
+            echo breadcrumbs();
+        ?>
+    </div>
+<?php endif; ?>
