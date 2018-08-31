398f6f804a0efcc44c4975d8990cad842fd9481e
Sean Jessen <info@seanjessen.co.uk>
2018-08-31 12:03:43 +0100

    Added new service component to shortlist

--- assets/src/js/components/Service.vue
+++ assets/src/js/components/Service.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="service">
-        <div class="service__image">
+        <div class="service__details" v-if="type === 'shortlist'">
+            <h4 class="service__name">{{ service.name }}</h4>
+            <p class="service__sub-title sm-copy">{{ organisation.name }}</p>
+            <p class="service__description sm-copy">{{ service.description }}</p>
+        </div>
+
+        <div class="service__image" v-if="type === 'service'">
             <img :src="`https://ck-api-staging.cloudapps.digital/core/v1/services/${service.slug}/logo`">
             
             <div class="service__add">
@@ -15,17 +21,36 @@
         <div class="service__location">
             <i class="fa fa-map-marker-alt"></i> <span class="service__location__name">Surbiton,Kingston</span>
         </div>
+
+        <div class="service__contact service__contact--telephone" v-if="type === 'shortlist' && service.contact_phone">
+            <span class="sm-copy">
+                <i class="fa fa-phone"></i> Telephone
+            </span>
+            <p>{{ service.contact_phone }}</p>
+        </div>
+
+        <div class="service__contact service__contact--email" v-if="type === 'shortlist' && service.contact_email">
+            <span class="sm-copy">
+                <i class="fa fa-envelope"></i> Email
+            </span>
+            <p>{{ service.contact_email }}</p>
+        </div>
         
-        <div class="service__meta">
+        <div class="service__meta" v-if="type === 'service'">
             <div class="service__meta__item sm-copy" v-if="service.is_free === true"><i class="fa fa-pound-sign"></i> Free</div>
             <div class="service__meta__item sm-copy" v-if="service.wait_time"><i class="fa fa-hourglass"></i> {{ returnWaitTime(service.wait_time) }}</div>
         </div>
 
-        <div class="service__details">
+        <div class="service__details" v-if="type === 'service'">
             <h4 class="service__name" v-if="service.name">{{ service.name }}</h4>
             <p class="service__sub-title sm-copy" v-if="organisation.name">{{ organisation.name }}</p>
             <p class="service__description sm-copy" v-if="service.description">{{ service.description }}</p>
             <a v-bind:href="['/services/' + service.slug]" role="button" class="btn btn--small">View more <i class="fa fa-angle-right"></i></a>
+        </div>
+
+        <div class="service__actions" v-if="type === 'shortlist'">
+            <a v-bind:href="['services/' + service.slug]" class="btn btn--small" role="button">View more <i class="fa fa-angle-right"></i></a>
+            <a v-on:click="removeFromShortlist" v-bind:data-id="service.id" class="btn btn--small btn--green" role="button">Remove <i class="fa fa-ban"></i></a>
         </div>
     </div>
 </template>
@@ -35,7 +60,7 @@
     
     export default {
         name: "service",
-        props: ['service'],
+        props: ['type', 'service'],
         data () {
             return {
                 organisation: null,
@@ -60,9 +85,25 @@
             addToShortlist(e) {
                 // Set shortlist cookie
                 this.$cookies.set("ck_shortlist", this.$cookies.get("ck_shortlist") + ',' + e.currentTarget.getAttribute('data-id'))
+            },
+            removeFromShortlist(e) {
+                console.log(e.currentTarget.getAttribute('data-id'), this.$cookies.get("ck_shortlist"))
+
+                // Set shortlist data
+                this.shortlist = this.shortlist.replace(e.currentTarget.getAttribute('data-id'), '')
+
+                // Set shortlist cookie
+                this.$cookies.set("ck_shortlist", this.shortlist)
+                
+                // retrieve new shortlist
+                this.$parent.updateShortlist()
             }
         },
         mounted () {
+            // Get the shortlist
+            this.shortlist = this.$cookies.get("ck_shortlist")
+
+            // Get the organisation
             this.getOrganisation()
         },
         computed: {
--- assets/src/js/components/ServicesShortlist.vue
+++ assets/src/js/components/ServicesShortlist.vue
@@ -8,36 +8,7 @@
 
         <div class="flex-container flex-container--justify">
             <div class="flex-col flex-col--4 flex-col--gutter" v-for="service in services.data" :key="service.id">
-                <div class="service">
-                    <div class="service__details">
-                        <h4 class="service__name">{{ service.name }}</h4>
-                        <p class="service__sub-title sm-copy">SPEAR</p>
-                        <p class="service__description sm-copy">{{ service.description }}</p>
-                    </div>
-
-                    <div class="service__location">
-                        <i class="fa fa-map-marker-alt"></i> <span class="service__location__name">Surbiton,Kingston</span>
-                    </div>
-
-                    <div class="service__contact service__contact--telephone" v-if="service.contact_phone">
-                        <span class="sm-copy">
-                            <i class="fa fa-phone"></i> Telephone
-                        </span>
-                        <p>{{ service.contact_phone }}</p>
-                    </div>
-
-                    <div class="service__contact service__contact--email" v-if="service.contact_email">
-                        <span class="sm-copy">
-                            <i class="fa fa-envelope"></i> Email
-                        </span>
-                        <p>{{ service.contact_email }}</p>
-                    </div>
-
-                    <div class="service__actions">
-                        <a v-bind:href="['services/' + service.slug]" class="btn btn--small" role="button">View more <i class="fa fa-angle-right"></i></a>
-                        <a v-on:click="removeFromShortlist" v-bind:data-id="service.id" class="btn btn--small btn--green" role="button">Remove <i class="fa fa-ban"></i></a>
-                    </div>
-                </div>
+                <service v-bind:type="'shortlist'" v-bind:service="service"></service>
             </div>
         </div>
     </section>
@@ -45,9 +16,13 @@
  
 <script>
     import axios from 'axios'
+    import Service from './Service'
     
     export default {
         name: "services-shortlist",
+        components: {
+            Service
+        },
         data () {
             return {
                 services: null,
@@ -55,19 +30,13 @@
             }
         },
         methods: {
-            removeFromShortlist(e) {
-                console.log(e.currentTarget.getAttribute('data-id'), this.$cookies.get("ck_shortlist"))
-
-                // Set shortlist data
-                this.shortlist = this.shortlist.replace(e.currentTarget.getAttribute('data-id'), '')
-
-                // Set shortlist cookie
-                this.$cookies.set("ck_shortlist", this.shortlist)
-                
-                // retrieve new shortlist
+            updateShortlist() {
+                // Retrieve items in shortlist
                 axios
                 .get('https://ck-api-staging.cloudapps.digital/core/v1/services?filter[id]=' + this.shortlist)
-                .then(response => (this.services = response.data))
+                .then(response => (
+                    this.services = response.data
+                ))
                 .catch(error => console.log(error))
             }
         },
@@ -75,11 +44,7 @@
             // Get the shortlist
             this.shortlist = this.$cookies.get("ck_shortlist")
             
-            // Retrieve items in shortlist
-            axios
-            .get('https://ck-api-staging.cloudapps.digital/core/v1/services?filter[id]=' + this.shortlist)
-            .then(response => (this.services = response.data))
-            .catch(error => console.log(error))
+            this.updateShortlist()
         }
     }
 </script>
