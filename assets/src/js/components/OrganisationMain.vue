71820bc3cf1e115bca278a3dda0759586333eb47
Sean Jessen <info@seanjessen.co.uk>
2018-08-31 12:41:15 +0100

    Added service component to organisations

--- assets/src/js/components/OrganisationMain.vue
+++ assets/src/js/components/OrganisationMain.vue
@@ -4,36 +4,7 @@
             <div class="flex-col flex-col--8">
                 <div class="flex-container flex-container--no-padding">
                     <div class="flex-col flex-col--6 flex-col--gutter" v-for="service in services" :key="service.id">
-                        <div class="service">
-                            <div class="service__image">
-                                <img :src="`https://ck-api-staging.cloudapps.digital/core/v1/services/${service.slug}/logo`">
-                                
-                                <div class="service__add">
-                                    <a href="#" role="button" class="btn btn--small">Add to shortlist <i class="fa fa-star"></i></a>
-                                </div>
-                            </div>
-
-                            <div class="service__location">
-                                <i class="fa fa-map-marker-alt"></i> <span class="service__location__name">Surbiton, Kingston</span>
-                            </div>
-                            
-                            <div class="service__meta">
-                                <div class="service__meta__item sm-copy">
-                                    <i class="fa fa-pound-sign"></i> Free
-                                </div>
-                                
-                                <div class="service__meta__item sm-copy">
-                                    <i class="fa fa-hourglass"></i> Up to 1 week
-                                </div>
-                            </div>
-
-                            <div class="service__details">
-                                <h4 class="service__name">{{ service.name }}</h4>
-                                <p class="service__sub-title sm-copy">{{ organisation.name }}</p>
-                                <p class="service__description sm-copy">{{ service.intro }}</p>
-                                <a v-bind:href="['/services/' + service.slug]" role="button" class="btn btn--small">View more <i class="fa fa-angle-right"></i></a>
-                            </div>
-                        </div>
+                        <service v-bind:type="'service'" v-bind:service="service"></service>
                     </div>
                 </div>
             </div>
@@ -93,9 +64,13 @@
  
 <script>
     import axios from 'axios'
+    import Service from './Service'
     
     export default {
         name: "organisation-main",
+        components: {
+            Service
+        },
         data () {
             return {
                 organisation: null,
@@ -120,7 +95,7 @@
             },
             getServices() {
                 axios
-                .get('https://ck-api-staging.cloudapps.digital/core/v1/services?filter[organisation_id]=' + this.$data.organisation.id + '&include=location')
+                .get('https://ck-api-staging.cloudapps.digital/core/v1/services?filter[organisation_id]=' + this.$data.organisation.id)
                 .then(response => (this.services = response.data.data))
                 .catch(error => console.log(error))
             }
