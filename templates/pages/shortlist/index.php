0468521914ea859d14fb0743af030e4f0b20358d
Sean Jessen <info@seanjessen.co.uk>
2018-09-02 18:20:33 +0100

    Tweaked shortlist template responsive

--- templates/pages/shortlist/index.php
+++ templates/pages/shortlist/index.php
@@ -1,10 +1,10 @@
 <section class="section section--reduce-padding section--header section--header--3">
-    <div class="flex-container flex-container--space">
+    <div class="flex-container flex-container--space flex-container--mobile-no-padding">
         <div class="flex-col">
             {% include('templates/_components/title-card.php') with { title: 'Shortlist', enableShare:'true' } %}
         </div>
 
-        <div class="flex-col text-center">
+        <div class="flex-col text-center tablet-hide">
             {% include('templates/_components/title-card.php') with { action: '#', actionText: 'Print services', actionIcon: 'fa fa-print', actionIconPosition: 'before' } %}
         </div>
     </div>
