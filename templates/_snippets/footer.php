122cd54f1a91c3a634d88537aa3857da3f62d58f
Sean Jessen <info@seanjessen.co.uk>
2018-09-11 10:27:29 +0100

    Updated global footer layout closes #104

--- templates/_snippets/footer.php
+++ templates/_snippets/footer.php
@@ -1,34 +1,9 @@
 <footer class="footer footer--<?php echo $CURRENT_PAGE; ?>">
   <div class="flex-container">
     <div class="flex-col flex-col--6 flex-col--tablet--12">
-      <div class="flex-container flex-container--no-padding">
-        <div class="flex-col flex-col--6 flex-col--mobile--6">  
-          <nav class="nav nav--footer" name="about-nav">
-            <label for="about-nav">About us</label>
-
-            <a href="#" class="nav__link">Lorem ipsum</a>
-            <a href="#" class="nav__link">Lorem ipsum</a>
-            <a href="#" class="nav__link">Lorem ipsum</a>
-          </nav>
-          
-          <nav class="nav nav--footer" name="about-nav">
-            <a href="#" class="nav__link">Terms and conditions</a>
-          </nav>
-        </div>
-  
-        <div class="flex-col flex-col--6 flex-col--mobile--6">  
-          <nav class="nav nav--footer" name="about-nav">
-            <label for="about-nav">Help</label>
-
-            <a href="#" class="nav__link">Lorem ipsum</a>
-            <a href="#" class="nav__link">Lorem ipsum</a>
-            <a href="#" class="nav__link">Lorem ipsum</a>
-          </nav>
-          
-          <nav class="nav nav--footer" name="about-nav">
-            <a href="#" class="nav__link">Privacy Policy</a>
-          </nav>
-        </div>
+      <div class="footer__disclaimer">
+        <p><strong>Disclaimer</strong></p>
+        <p class="sm-copy">Kingston Voluntary Action (KVA) on behalf of the Royal Borough of Kingston Council makes every effort to provide up-to-date and accurate information on this website. However, KVA and other parties involved in creating and delivering this website's contents do not accept any liability for any loss or damage arising in any way whatsoever from the use of information on this website. The listing of an organisation and the option to make referrals online in no way should be viewed as an endorsement or recommendation</p>
       </div>
     </div>
 
@@ -46,25 +21,21 @@
         <div class="flex-col flex-col--6 text-right text-left--mobile">
           <a href="https://admin.connectedkingston.uk" class="btn btn--secondary btn--dark">Member area</a>
         </div>
+      </div>
 
-        <div class="flex flex-col--12">
-          <div class="footer__disclaimer">
-            <p><strong>Disclaimer</strong></p>
-            <p class="sm-copy">Kingston Voluntary Action (KVA) on behalf of the Royal Borough of Kingston Council makes every effort to provide up-to-date and accurate information on this website. However, KVA and other parties involved in creating and delivering this website's contents do not accept any liability for any loss or damage arising in any way whatsoever from the use of information on this website. The listing of an organisation and the option to make referrals online in no way should be viewed as an endorsement or recommendation</p>
-          </div>
-
-          <!-- <div class="footer__sponsors">
-            <img src="https://picsum.photos/91/91" alt="">
-            <img src="https://picsum.photos/91/110" alt="">
-            <img src="https://picsum.photos/91/91" alt="">
-          </div> -->
+      <div class="flex-container flex-container--no-padding">
+        <div class="flex-col flex-col--6 flex-col--mobile--6">
+          <nav class="nav nav--footer" name="about-nav">
+            <a href="#" class="nav__link">Terms and conditions</a>
+          </nav>
+        </div>
+  
+        <div class="flex-col flex-col--6 flex-col--mobile--6">            
+          <nav class="nav nav--footer" name="about-nav">
+            <a href="#" class="nav__link">Privacy Policy</a>
+          </nav>
         </div>
       </div>
     </div>
-
-    <!-- <div class="footer__disclaimer flex-col flex-col--6 flex-col--tablet--12">
-      <p><strong>Disclaimer</strong></p>
-      <p class="sm-copy">Kingston Voluntary Action (KVA) on behalf of the Royal Borough of Kingston Council makes every effort to provide up-to-date and accurate information on this website. However, KVA and other parties involved in creating and delivering this website's contents do not accept any liability for any loss or damage arising in any way whatsoever from the use of information on this website. The listing of an organisation and the option to make referrals online in no way should be viewed as an endorsement or recommendation</p>
-    </div> -->
   </div>
 </footer>
