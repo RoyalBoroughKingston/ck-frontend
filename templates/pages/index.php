65944c38ec02c0aedf75da2dfb673c83db26b3c6
Mike Stephens <mike@ayup.agency>
2018-09-26 20:44:39 +0100

    Replaced homepage image with a video

--- templates/pages/index.php
+++ templates/pages/index.php
@@ -57,7 +57,7 @@
 <section class="section mobile-hide">
     <div class="flex-container">
         <div class="flex-col flex-col--7 highlight highlight--grey">
-            {% include('templates/_components/media.php') with { type: 'image', imageUrl: '/assets/dist/img/global/CK_Homepage_About.jpg', imageTitle: 'Connected Kingston' } %}
+            {% include('templates/_components/media.php') with { type: 'video', embedCode: '<iframe width="560" height="315" src="https://www.youtube.com/embed/h-2sgpokvGI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>' } %}
         </div>
 
         <div class="flex-col flex-col--5">
